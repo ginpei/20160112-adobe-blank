747908c626ed101c91347d654444230648d34682
ginpei <ginpei@gmail.com>
2016-01-13 13:14:17 -0800

    change interfaces

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 		var elStatus = document.querySelector('.js-status');
 		elStatus.textContent = 'Loading...';
 
-		checker.start({
+		check({
 			fontName: 'Source Code Pro',
 			onload: function() {
 				elStatus.textContent = 'Loaded.';
@@ -14,54 +14,81 @@
 	// ----------------------------------------------------------------
 	// Checker
 
-	var checker = {
+	function check(options) {
+		var checker = new Checker(options);
+		checker.start();
+		return checker;
+	}
+
+	function Checker(options) {
+		this.initialize(options);
+	}
+	(function(prototype) {
+		var p = Checker.prototype;
+		for (var key in prototype) {
+			p[key] = prototype[key];
+		}
+	})({
 		elChecker: null,
 		tmCheck: null,
+
+		fontName: null,
+		interval: null,
+		onload: null,
 
 		defaults: {
 			interval: 250
 		},
 
-		start: function(options) {
-			options = options || {};
-			options.interval = options.interval || this.defaults.interval;
-			this.prepareChecker(options);
-			this.tmCheck = window.setInterval(this.check.bind(this, options), options.interval);
+		initialize: function(options) {
+			this.fontName = options.fontName;
+			this.interval = options.interval || this.defaults.interval;
+			this.onload = options.onload;
 		},
 
-		prepareChecker: function(options) {
+		start: function() {
+			this.prepareChecker();
+			this.tmCheck = window.setInterval(this.check.bind(this), this.interval);
+		},
+
+		prepareChecker: function() {
 			var el = document.createElement('div');
 			el.textContent = 'X';
 			el.style.fontFamily = 'AdobeBlank';
 			el.style.position = 'absolute';
+
+			// WIP
 			// el.style.left = '-9999px';
 			el.style.top = '0px';
+
 			el.style.border = 'solid 1px #f00';
 			document.body.appendChild(el);
 
 			this.elChecker = el;
 
+			// make sure the checker is rendered using Adobe Bank once
 			setTimeout(function() {
-				el.style.fontFamily = '"' + options.fontName + '", AdobeBlank';
+				el.style.fontFamily = '"' + this.fontName + '", AdobeBlank';
 			}.bind(this), 1);
 		},
 
-		check: function(options) {
+		check: function() {
 			var width = this.elChecker.clientWidth;
 			if (width !== 0) {
-				this.clean();
-				options.onload();
+				this.destroy();
+				this.onload();
 			}
 		},
 
-		clean: function() {
+		destroy: function() {
 			window.clearInterval(this.tmCheck);
+
 			// WIP
 			// document.body.removeChild(this.elChecker);
 
 			this.elChecker = this.tmCheck = null;
 		}
-	};
+	});
 
 	// ----------------------------------------------------------------
 
